4038011da1c62a78c639454ff3728001ebddf9e1
noxend <noxios5674@gmail.com>
2021-05-04 19:50:25 +0300

    animation component

--- src/core/components/Animation.ts
+++ src/core/components/Animation.ts
@@ -1,4 +1,4 @@
-import { range } from "../../utils";
+import { range, createElement } from "../../utils";
 import { Component } from "../Component";
 import { Sprite } from "./Sprite";
 
@@ -7,55 +7,73 @@ type Frames = {
   to: number;
 };
 
+const currentAnimationDebug = createElement("animation");
 export class Animation extends Component {
-  public frames: Frames;
-  public animations: any;
-  public default: string;
+  public readonly frames: Frames;
+  public readonly animations: any;
+  public readonly default: string;
 
-  private lastTime = performance.now();
-  private index = 0;
+  public readonly animations2: Map<string, any>;
+
+  private lastTime: number;
   private sprite: Sprite;
+  private index = 0;
 
-  private framesRange: ReadonlyArray<number>;
-
+  private current: string;
   private old: string;
 
   init() {
     this.sprite = this.entity.getComponent(Sprite) as Sprite;
-    this.framesRange = range(this.frames.from, this.frames.to);
+    this.old = this.current = this.default;
 
-    this.old = this.default;
+    this.lastTime = performance.now();
+
+    for (const item in this.animations) {
+      const anim = this.animations[item];
+
+      if (anim.frames || anim.from === undefined || anim.to === undefined)
+        continue;
+
+      this.animations[item].frames = range(anim.from, anim.to);
+    }
+  }
+
+  public setAnimationByName(name: string) {
+    this.current = name;
   }
 
   private nextFrame(): void {
-    const animation = this.animations[this.default];
-    const range = animation.range;
+    const animation = this.animations[this.current];
+    const frames = animation.frames;
     const repeat = animation.repeat;
 
-    const isLastFrame = range[range.length - 1] === range[this.index];
-
-    if (isLastFrame && !repeat) return this.sprite.setSprite(range[this.index]);
+    const isLastFrame = frames[frames.length - 1] === frames[this.index];
 
     if (isLastFrame) this.index = repeat ? 0 : this.index;
     else this.index += 1;
 
-    this.sprite.setSprite(range[this.index]);
+    this.sprite.setSprite(frames[this.index]);
   }
 
   update() {
     const time = performance.now();
 
-    if (this.default !== this.old) {
+    if (!this.current) this.current = this.default;
+
+    if (this.current !== this.old) {
+      currentAnimationDebug.textContent = this.current;
       this.index = 0;
-      this.old = this.default;
+      this.old = this.current;
     }
 
     if (time - this.lastTime > 120) {
       this.nextFrame();
       this.lastTime = time;
     } else {
-      this.sprite.setSprite(this.animations[this.default].range[this.index]);
+      this.sprite.setSprite(this.animations[this.current].frames[this.index]);
     }
+
+    this.current = "";
   }
 }
 
