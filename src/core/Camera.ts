90d74d4cdd480e00dd9e9a71a3cffe48d178fe09
noxend <noxios5674@gmail.com>
2021-05-13 22:16:19 +0300

    first variant

--- src/core/Camera.ts
+++ src/core/Camera.ts
@@ -1,16 +1,23 @@
-import { Entity } from "core/Entity";
-import Vector from "utils/Vector";
+import Vector from "../utils/Vector";
+import { Component } from "./Component";
 
-export class Camera {
-  public position: Vector;
+export class Camera extends Component {
+  public position: Vector = new Vector();
 
-  constructor(private entity: Entity) {
-    this.position = new Vector();
-  }
+  public update() {
+    const entities = this.entity.manager.entities;
+    const canvasWidth = window.ctx.canvas.width;
+    const canvasHeight = window.ctx.canvas.height;
 
-  public update(dt: number) {
-    const w = window.ctx.canvas.width;
+    this.position.x = this.entity.position.x - canvasWidth / 2;
+    this.position.y = this.entity.position.y - canvasHeight / 2;
 
-    this.position.x = this.entity.position.x - w / 2;
+    for (let i = 0; i < entities.length; i++) {
+      const entity = entities[i];
+
+      // entity.position = Vector.lerp(this.position, entity.position, 0.2);
+      entity.position.x -= this.position.x;
+      entity.position.y -= this.position.y;
+    }
   }
 }
