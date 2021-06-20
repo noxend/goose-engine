c05ca638637f421688c8ce9f20e45d92b05e41f5
noxend <noxios5674@gmail.com>
2021-06-20 10:07:30 +0300

    demo game

---
+++ src/demo/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as hero } from "./hero.png";
+export { default as goblin } from "./goblin.png";
+export { default as tileset } from "./tileset.png";
+export { default as coin } from "./coin.png";
+
+export { default as jump } from "./jump.wav";
