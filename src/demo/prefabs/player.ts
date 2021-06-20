acbf4d0e58b15fc2499ed9261e836ca47b2c47f1
noxend <noxios5674@gmail.com>
2021-06-20 10:00:31 +0300

    absolute import

--- src/demo/prefabs/player.ts
+++ src/demo/prefabs/player.ts
@@ -1,8 +1,8 @@
-import { Animation } from "../../core/components/Animation";
-import { Sprite } from "../../core/components/Sprite";
-import { Entity } from "../../core/Entity";
+import { Animation } from "@/core/components/Animation";
+import { Sprite } from "@/core/components/Sprite";
+import { Entity } from "@/core/Entity";
 
-import Vector from "../../utils/Vector";
+import Vector from "@/utils/Vector";
 
 const getPlayer = async (): Promise<Entity> => {
   const player = new Entity("player", new Vector());
