66467778c10f98082fce7b5a7a0ecc66f8ac113f
noxend <noxios5674@gmail.com>
2021-06-20 09:48:34 +0300

    player prefab

---
+++ src/demo/prefabs/player.ts
@@ -0,0 +1,31 @@
+import { Animation } from "../../core/components/Animation";
+import { Sprite } from "../../core/components/Sprite";
+import { Entity } from "../../core/Entity";
+
+import Vector from "../../utils/Vector";
+
+const getPlayer = async (): Promise<Entity> => {
+  const player = new Entity("player", new Vector());
+
+  player.addComponent(Sprite, { spriteSize: new Vector(16, 16) });
+
+  player.addComponent(Animation, {
+    default: "idle",
+    animations: {
+      idle: {
+        repeat: true,
+        from: 40,
+        to: 43,
+      },
+      run: {
+        repeat: true,
+        from: 8,
+        to: 13,
+      },
+    },
+  });
+
+  return player;
+};
+
+export default getPlayer;
